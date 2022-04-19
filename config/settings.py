47154f249d42e06c90eb74f8f13da327bdcaa8a1
Taltal <taltalrealty@gmail.com>
2022-04-19 15:55:56 +0900

    .

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ DEBUG = bool(os.environ.get("DEBUG"))
 # DEBUG = True
 
 
-ALLOWED_HOSTS = [".elasticbeanstalk.com", 'localhost', "127.0.0.1", '.ap-northeast-2.compute.amazonaws.com']
+ALLOWED_HOSTS = [".elasticbeanstalk.com", 'localhost', "127.0.0.1", '.ap-northeast-2.compute.amazonaws.com', 'taltalrealty.ga']
 
 
 # Application definition
