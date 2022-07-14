d487ae3a8a477c91b2e744eca701f04ad0990ede
Taltal <taltalrealty@gmail.com>
2022-07-14 14:21:28 +0900

    .

--- apis/views.py
+++ apis/views.py
@@ -118,7 +118,7 @@ def kakao_login_app(request):
         REDIRECT_URI = f"{home_url}/api/v1/login/kakao/callback/"
     else:
         REST_API_KEY = os.environ.get("KAKAO_ID_DEPLOY")
-        REDIRECT_URI = "http://taltalrealty31-dev.ap-northeast-2.elasticbeanstalk.com/api/v1/users/social-login/"
+        REDIRECT_URI = "http://taltalrealty31-dev.ap-northeast-2.elasticbeanstalk.com/api/v1/login/kakao/callback/"
     return redirect(f"https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={REDIRECT_URI}&response_type=code")
 
 
