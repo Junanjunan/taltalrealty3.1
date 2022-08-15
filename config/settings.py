db8259e3fb52457e4339ac83763a063cb69161e5
Taltal <taltalrealty@gmail.com>
2022-08-15 16:51:32 +0900

    .:

--- config/settings.py
+++ config/settings.py
@@ -167,7 +167,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-# DEFAULT_FROM_EMAIL = 'wnsghksk@gmail.com'
+
 
 
 # Sentry
