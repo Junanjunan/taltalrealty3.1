cbc537baf3976a19db34400c8ad9918af1568a95
Taltal <taltalrealty@gmail.com>
2022-08-15 17:03:44 +0900

    .

--- config/settings.py
+++ config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET", "fjzmdlazhk!2jdkz3")
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
 # SECRET_KEY = "1k2kdmahjsloqqjkdfasl"
 
 # SECURITY WARNING: don't run with debug turned on in production!
