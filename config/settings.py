9bb5b7917cddd0c2ef38e53effa3af6ca77974aa
Junanjunan <taltalrealty@gmail.com>
2022-12-04 21:44:20 +0900

    3.2 version default settings.py

--- config/settings.py
+++ config/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
+from .local_settings import DJANGO_SECRET, DEBUG_CONFIG, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET")
-# SECRET_KEY = "1k2kdmahjsloqqjkdfasl"
+SECRET_KEY = DJANGO_SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG"))
-# DEBUG = False
+DEBUG = DEBUG_CONFIG
 
 
 ALLOWED_HOSTS = ["*"]
@@ -53,7 +50,7 @@ PROJECT_APPS = [
     "apis.apps.ApisConfig"
 ]
 
-THIRD_PARTY_APPS = ['import_export', 'verify_email', "storages", "rest_framework"]
+THIRD_PARTY_APPS = []
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
@@ -91,25 +88,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            "HOST": os.environ.get("RDS_HOST"),
-            "NAME": os.environ.get("RDS_NAME"),
-            "USER": os.environ.get("RDS_USER"),
-            "PASSWORD": os.environ.get("RDS_PASSWORD"),
-            "PORT": "5432",
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 
 # Password validation
@@ -153,44 +138,20 @@ AUTH_USER_MODEL = 'users.User'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS =  [os.path.join(BASE_DIR, "static"),]
-STATIC_ROOT = os.path.join(BASE_DIR, "var", "static")
+# STATIC_ROOT = os.path.join(BASE_DIR, "var", "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "uploads")
 MEDIA_URL = "/media/"
 
 
 # email verification
-
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
 
-
-
-# Sentry
-if not DEBUG:
-    DEFAULT_FILE_STORAGE = 'config.custom_storages.UploadStorage'
-    STATICFILES_STORAGE = 'config.custom_storages.StaticStorage'
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_STORAGE_BUCKET_NAME = 'taltalrealty31'
-    AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86500'}
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com'     # AWS_STORAGE_BUCKET_NAME 위치가 뒤에 와야 되는 경우도 있음 / 상황에 따라 다르니 주의 -> S3 들어가서 url 보고 확인
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
-
-    sentry_sdk.init(
-    dsn=os.environ.get("SENTRY_URL"),
-    integrations=[DjangoIntegration()],
-    send_default_pii=True,
-    )
-
-
-# WARNINGS: books.ApartmentDealing: (models.W042) Auto-created primary key used when not defining a primary key type, by default 'django.db.models.AutoField'. / HINT: Configure the DEFAULT_AUTO_FIELD setting or the BooksConfig.default_auto_field attribute to point to a subclass of AutoField, e.g. 'django.db.models.BigAutoField'.
-DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 # Django Rest Framework
 REST_FRAMEWORK = {
@@ -201,7 +162,4 @@ REST_FRAMEWORK = {
 }
 
 
-# AUTHENTICATION_BACKENDS = (
-#     # ... your other backends
-#     'config.auth_backend.PasswordlessAuthBackend',
-# )
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
