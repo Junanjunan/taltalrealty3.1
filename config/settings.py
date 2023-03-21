4bd530aaa51115c6d356668de9137cff4dbdb3b3
taltal <taltalrealty@gmail.com>
2023-03-22 01:39:00 +0900

    Change annotation to code

--- config/settings.py
+++ config/settings.py
@@ -140,7 +140,7 @@ MEDIA_URL = "/media/"
 
 
 # email verification
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
